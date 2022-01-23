6069132426f5d9006c92bc031fabc79552d9af80
Augusto <augustohansel@hotmail.com>
2022-01-22 22:49:50 -0300

    add post to /sign-up

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
 
 const server = express();
+server.use(express.json());
+server.use(cors());
+const user = {
+  username: '',
+  avatar: '',
+};
+const allUsers = [];
 
+server.post('/sign-up', (request, response) => {
+  user.username = request.body.username;
+  user.avatar = request.body.avatar;
+  allUsers.push(user);
+  console.log(allUsers);
+  response.send('OK');
+});
 
 server.listen(5000);
